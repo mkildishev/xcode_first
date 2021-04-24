b32af8f4b1d8eb870ab130a16e2edc4d03b92945
Maksim Kildishev <thanatormax@gmail.com>
2021-04-24 00:58:44 -0700

    feat: add SecondViewController

---
+++ RSSchoolClient/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  RSSchoolClient
+//
+//  Created by Maksim on 24.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
